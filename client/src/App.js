cbd21643f188aaec8a33c744e9fa40709b94ad9b
AmanyFekry <amanyfekry200045@gmail.com>
2021-08-18 01:33:09 +0200

    ShoppingList Components

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,23 @@
 import React,{ Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppNavbar from './components/AppNavbar';
+import ShoppingList from './components/ShoppingList';
 import './App.css';
+import {Container} from 'reactstrap';
+
 
 class App extends Component{
 render() {
   return (
     <div className="App">
      <AppNavbar />
+     <Container>
+     <ShoppingList/>
+     </Container> 
     </div>
   );
-}
+
+ }
 }
 
 export default App;
