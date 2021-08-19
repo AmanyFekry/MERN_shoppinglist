d3dd6234b0e85def235e9aea55e4b917c2b53c64
AmanyFekry <amanyfekry200045@gmail.com>
2021-08-20 00:41:27 +0200

    Using Redux to manage state which you can access the state from any other component that you create

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,10 @@ import React,{ Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
+
+import {Provider} from 'react-redux';
+import store from './store';
+
 import './App.css';
 import {Container} from 'reactstrap';
 
@@ -9,12 +13,15 @@ import {Container} from 'reactstrap';
 class App extends Component{
 render() {
   return (
+    < Provider store={store}>
     <div className="App">
      <AppNavbar />
      <Container>
      <ShoppingList/>
      </Container> 
     </div>
+    
+    </Provider> 
   );
 
  }
