97623d3364f985b40d25c7e220232a0ea8f08328
AmanyFekry <amanyfekry200045@gmail.com>
2021-08-20 02:25:42 +0200

    creating a new component to add som items on shoppinglist with redux-react

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React,{ Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
-
+import ItemModal from './components/itemModal';
 import {Provider} from 'react-redux';
 import store from './store';
 
@@ -16,6 +16,7 @@ render() {
     < Provider store={store}>
     <div className="App">
      <AppNavbar />
+     < ItemModal/>
      <Container>
      <ShoppingList/>
      </Container> 
